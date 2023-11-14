56ce0c7d8ac0ebac3c2aac8f09e366e9b32b026e
Iara <loure.iara.s@gmail.com>
2023-11-14 10:08:58 -0300

    feat(tb): valores negativos jamais

--- prova2.sql
+++ prova2.sql
@@ -1,4 +1,24 @@
 --Prova 2
+--2 Trigger--
+CREATE OR REPLACE TRIGGER tg_antes_insert_update
+BEFORE INSERT OR UPDATE ON tb_youtube
+FOR EACH ROW
+EXECUTE PROCEDURE fn_antes_insert_update('Antes: V1', 'Antes: V2');
+
+CREATE OR REPLACE FUNCTION fn_antes_insert_update()
+RETURNS TRIGGER
+LANGUAGE plpgsql AS $$
+BEGIN
+	IF NEW.subscribers >= 0 AND NEW.video_views >= 0 AND NEW.video_count >= 0 THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Valores numéricos inválidos: subscribers=%, video_views=%, video_count=%',
+                        NEW.subscribers, NEW.video_views, NEW.video_count;
+        RETURN NULL;
+    END IF;
+END;
+$$
+
 
 --testando
 SELECT * FROM tb_youtube
